436989ebc198b5fb6014bedbe563535dd08994ec
seiryuu <draxasyt@gmail.com>
2025-03-26 23:55:00 +0100

    formular a db

--- contact.php
+++ contact.php
@@ -58,30 +58,24 @@ include_once 'parts/head.php';
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                     <h3 class="mb-4">Need a functional contact form?</h3>
                     <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                    <form>
+                    <form id="contact" method="post" action="db/spracovanieFormulara.php">
                         <div class="row g-3">
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                    <label for="name">Your Name</label>
+                                    <input type="text" class="form-control" id="name" placeholder="Meno" name="meno" >
+                                    <label for="name">Meno</label>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                    <label for="email">Your Email</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                                    <label for="email">Email</label>
                                 </div>
                             </div>
                             <div class="col-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    <label for="subject">Subject</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 120px"></textarea>
-                                    <label for="message">Message</label>
+                                    <textarea class="form-control" placeholder="Sem napíšte vašu správu" id="message" name="sprava" style="height: 120px"></textarea>
+                                    <label for="message">Sem napíšte vašu správu</label>
                                 </div>
                             </div>
                             <div class="col-12">
---
+++ db/spracovanieFormulara.php
@@ -0,0 +1,34 @@
+<?php
+// PDO databázové pripojenie
+$host = "localhost";
+$dbname = "cajovnaFormular";
+$port = 3306;
+$username = "root";
+$password = "";
+
+
+// Pripojenie PDO
+try {
+    $conn = new PDO('mysql:host='.$host.';dbname='.$dbname.";port=".$port, $username,
+        $password);
+} catch (PDOException $e) {
+    die("Chyba pripojenia: " . $e->getMessage());
+}
+
+// Získanie údajov z formulára
+$meno = $_POST["meno"];
+$email = $_POST["email"];
+$sprava = $_POST["sprava"];
+// SQL príkaz INSERT
+$sql = "INSERT INTO udaje (meno, email, sprava) 
+    VALUE ('".$meno."', '".$email."', '".$sprava."')";
+$statement = $conn->prepare($sql);
+try {
+    $insert = $statement->execute();
+    header("Location: http://localhost/cajovna/index.php"); // THANK YOU STRANKA
+    return $insert;
+} catch (\Exception $exception) {
+    return false;
+}
+// Zatvorenie pripojenia
+$conn = null;
